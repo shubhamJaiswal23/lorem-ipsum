7ef82a87c4ed0fc59ca45b6ae89051ddede25dab
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2024-01-08 16:48:08 +0530

    lorem-ipsum generator project complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,42 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import data from './data';
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = +count;
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h4>Tired of using Dummy Lorem Ipsum?</h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+          min="1"
+          step="1"
+          max="8"
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item) => {
+          return <p key={nanoid()}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 };
 export default App;
